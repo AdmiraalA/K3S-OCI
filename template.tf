a6284ae7af54ca14ca50357484afd491dcdf4ac2
Admiral <d.vanoosterhoud@gmail.com>
2024-02-21 17:05:19 +0100

    Update template.tf
    Enabled in-transit encryption, add OCI cloud agent parameters previously missing

--- template.tf
+++ template.tf
@@ -1,12 +1,11 @@
 resource "oci_core_instance_configuration" "k3s_server_template" {
-
   compartment_id = var.compartment_ocid
   display_name   = "k3s server configuration"
 
   freeform_tags = {
     "provisioner"           = "terraform"
-    "environment"           = "${var.environment}"
-    "${var.unique_tag_key}" = "${var.unique_tag_value}"
+    "environment"           = var.environment
+    "${var.unique_tag_key}" = var.unique_tag_value
     "k3s-template-type"     = "k3s-server"
   }
 
@@ -14,13 +13,13 @@ resource "oci_core_instance_configuration" "k3s_server_template" {
     instance_type = "compute"
 
     launch_details {
-
-      agent_config {
-        is_management_disabled = "false"
-        is_monitoring_disabled = "false"
+        is_pv_encryption_in_transit_enabled = true
+        agent_config {
+        is_management_disabled = false
+        is_monitoring_disabled = false
 
         plugins_config {
-          desired_state = "DISABLED"
+          desired_state = "ENABLED"
           name          = "Vulnerability Scanning"
         }
 
@@ -47,14 +46,15 @@ resource "oci_core_instance_configuration" "k3s_server_template" {
       display_name = "k3s server template"
 
       metadata = {
-        "ssh_authorized_keys" = file(var.public_key_path)
+        "ssh_authorized_keys" = var.ssh_authorized_keys_content
         "user_data"           = data.cloudinit_config.k3s_server_tpl.rendered
+        "OCI_METADATA_LEGACY" = "false"  # Disable metadata V1 endpoint
       }
 
       shape = var.compute_shape
       shape_config {
-        memory_in_gbs = "6"
-        ocpus         = "1"
+        memory_in_gbs = 6
+        ocpus         = 1
       }
       source_details {
         image_id    = var.os_image_id
@@ -65,14 +65,13 @@ resource "oci_core_instance_configuration" "k3s_server_template" {
 }
 
 resource "oci_core_instance_configuration" "k3s_worker_template" {
-
   compartment_id = var.compartment_ocid
   display_name   = "k3s worker configuration"
 
   freeform_tags = {
     "provisioner"           = "terraform"
-    "environment"           = "${var.environment}"
-    "${var.unique_tag_key}" = "${var.unique_tag_value}"
+    "environment"           = var.environment
+    "${var.unique_tag_key}" = var.unique_tag_value
     "k3s-template-type"     = "k3s-worker"
   }
 
@@ -80,10 +79,10 @@ resource "oci_core_instance_configuration" "k3s_worker_template" {
     instance_type = "compute"
 
     launch_details {
-
-      agent_config {
-        is_management_disabled = "false"
-        is_monitoring_disabled = "false"
+        is_pv_encryption_in_transit_enabled = true
+        agent_config {
+        is_management_disabled = false
+        is_monitoring_disabled = false
 
         plugins_config {
           desired_state = "DISABLED"
@@ -113,19 +112,20 @@ resource "oci_core_instance_configuration" "k3s_worker_template" {
       display_name = "k3s worker template"
 
       metadata = {
-        "ssh_authorized_keys" = file(var.public_key_path)
+        "ssh_authorized_keys" = var.ssh_authorized_keys_content
         "user_data"           = data.cloudinit_config.k3s_worker_tpl.rendered
+        "OCI_METADATA_LEGACY" = "false"  # Disable metadata V1 endpoint
       }
 
       shape = var.compute_shape
       shape_config {
-        memory_in_gbs = "6"
-        ocpus         = "1"
+        memory_in_gbs = 6
+        ocpus         = 1
       }
       source_details {
         image_id    = var.os_image_id
         source_type = "image"
       }
     }
-  }
+}
 }
