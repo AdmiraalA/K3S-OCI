5e40b752a499e809c7cce30e89d6f365408103a0
Admiral <d.vanoosterhoud@gmail.com>
2024-02-21 09:06:08 +0100

    Update provider.tf
    Added provider and var for Cloudflare DNS

--- oci/provider.tf
+++ oci/provider.tf
@@ -3,8 +3,22 @@ terraform {
     oci = {
       source  = "oracle/oci"
       version = ">= 4.64.0"
-    }
   }
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 4.0"
 }
-
-provider "oci" {}
+}
+}
+provider "oci" {
+  # Configuration for Oracle Cloud Infrastructure provider
+  tenancy_ocid     = var.tenancy_ocid
+  user_ocid        = var.user_ocid
+  private_key      = var.private_key
+  fingerprint      = var.fingerprint
+  region           = var.region
+}
+provider "cloudflare" {
+  # Configuration for Cloudflare provider
+ api_token = var.cloudflare_api_key
+}
